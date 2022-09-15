21d0daa31085403d6bb7249f9fed371ebff77a6b
Andrew Jeffery <dev@jeffas.io>
2022-09-15 13:51:16 +0100

    Add ci checks from CCF (#24)
    * Add proto formatting to .clang-format
    
    * Add ci-checks.sh script from CCF and corresponding workflow
    
    * Make ci-checks.sh executable
    
    * Add missing scripts for ci checks
    
    * Fix lints from ci-checks

---
+++ scripts/ci-checks.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache 2.0 License.
+
+set -e
+
+if [ "$1" == "-f" ]; then
+  FIX=1
+else
+  FIX=0
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ROOT_DIR=$( dirname "$SCRIPT_DIR" )
+pushd "$ROOT_DIR" > /dev/null
+
+CHECK_DELIMITER="---------------------------"
+
+echo "$CHECK_DELIMITER"
+echo "-- Shell scripts"
+git ls-files | grep -e '\.sh$' | grep -E -v "^3rdparty" | xargs shellcheck -s bash -e SC2044,SC2002,SC1091,SC2181
+
+echo "$CHECK_DELIMITER"
+echo "-- TODOs"
+"$SCRIPT_DIR"/check-todo.sh include src
+
+echo "$CHECK_DELIMITER"
+echo "-- C/C++/Proto format"
+if [ $FIX -ne 0 ]; then
+  "$SCRIPT_DIR"/check-format.sh -f include src samples
+else
+  "$SCRIPT_DIR"/check-format.sh include src samples
+fi
+
+echo "$CHECK_DELIMITER"
+echo "-- TypeScript, JavaScript, Markdown, YAML and JSON format"
+npm install --loglevel=error --no-save prettier 1>/dev/null
+if [ $FIX -ne 0 ]; then
+  git ls-files | grep -e '\.ts$' -e '\.js$' -e '\.md$' -e '\.yaml$' -e '\.yml$' -e '\.json$' | xargs npx prettier --write
+else
+  git ls-files -- . ':!:3rdparty/' | grep -e '\.ts$' -e '\.js$' -e '\.md$' -e '\.yaml$' -e '\.yml$' -e '\.json$' | xargs npx prettier --check
+fi
+
+echo "$CHECK_DELIMITER"
+echo "-- Copyright notice headers"
+python3.8 "$SCRIPT_DIR"/notice-check.py
+
+echo "$CHECK_DELIMITER"
+echo "-- CMake format"
+if [ $FIX -ne 0 ]; then
+  "$SCRIPT_DIR"/check-cmake-format.sh -f cmake samples src tests CMakeLists.txt
+else
+  "$SCRIPT_DIR"/check-cmake-format.sh cmake samples src tests CMakeLists.txt
+fi
+
+echo "$CHECK_DELIMITER"
+echo "-- Python dependencies"
+# Virtual Environment w/ dependencies for Python steps
+if [ ! -f "scripts/env/bin/activate" ]
+    then
+        python3.8 -m venv scripts/env
+fi
+
+source scripts/env/bin/activate
+pip install -U pip
+pip install -U wheel black pylint mypy 1>/dev/null
+
+echo "$CHECK_DELIMITER"
+echo "-- Python format"
+if [ $FIX -ne 0 ]; then
+  git ls-files scripts/ | grep -e '\.py$' | xargs black
+else
+  git ls-files scripts/ | grep -e '\.py$' | xargs black --check
+fi
