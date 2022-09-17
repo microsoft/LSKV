ff18be71811086f386d68e0153fb51f90a55f360
Andrew Jeffery <dev@jeffas.io>
2022-09-17 08:13:36 +0100

    Add license header

--- src/app/kvstore.h
+++ src/app/kvstore.h
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #include "ccf/app_interface.h"
 #include "ccf/common_auth_policies.h"
 #include "ccf/http_query.h"
