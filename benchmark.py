eb0331f73789ac5f9bfe2030db36a526bfd5af2c
Andrew Jeffery <dev@jeffas.io>
2022-09-21 18:01:56 +0100

    Write output to files

--- benchmark.py
+++ benchmark.py
@@ -6,6 +6,7 @@ from subprocess import Popen
 import sys
 import time
 import socket
+import os
 from typing import List
 
 ETCD = "etcd"
@@ -13,6 +14,8 @@ CCF_KVS = "ccf_kvs"
 
 ETCD_BENCH_CMD = ["bin/benchmark", "--endpoints", "http://127.0.0.1:8000"]
 CCF_KVS_BENCH_CMD = ["bin/benchmark", "--endpoints", "https://127.0.0.1:8000", "--cacert", "workspace/sandbox_common/service_cert.pem", "--cert", "workspace/sandbox_common/user0_cert.pem", "--key", "workspace/sandbox_common/user0_privk.pem", "put"]
+
+BENCH_DIR = "bench"
 
 def wait_for_port(port=8000):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,19 +41,27 @@ class Store:
 class EtcdStore:
     def spawn(self)->Popen:
         client_urls = "http://127.0.0.1:8000"
-        return Popen(["bin/etcd", "--listen-client-urls", client_urls, "--advertise-client-urls", client_urls], stdout=sys.stdout, stderr=sys.stderr)
+        with open(os.path.join(BENCH_DIR, "etcd.out"), 'w') as out:
+            with open(os.path.join(BENCH_DIR, "etcd.err"), 'w') as err:
+                return Popen(["bin/etcd", "--listen-client-urls", client_urls, "--advertise-client-urls", client_urls], stdout=out, stderr=err)
 
     def bench(self, bench_cmd:List[str]):
-        p = Popen(ETCD_BENCH_CMD+bench_cmd, stdout=sys.stdout, stderr=sys.stderr)
-        p.wait()
+        with open(os.path.join(BENCH_DIR, "etcd_bench.out"), 'w') as out:
+            with open(os.path.join(BENCH_DIR, "etcd_bench.err"), 'w') as err:
+                p = Popen(ETCD_BENCH_CMD+bench_cmd, stdout=out, stderr=err)
+                p.wait()
 
 class CCFKVSStore:
     def spawn(self)->Popen:
-        return Popen(["/opt/ccf/bin/sandbox.sh", "-p", "build/libccf_kvs.virtual.so", "--http2"], stdout=sys.stdout, stderr=sys.stderr)
+        with open(os.path.join(BENCH_DIR, "ccf_kvs.out"), 'w') as out:
+            with open(os.path.join(BENCH_DIR, "ccf_kvs.err"), 'w') as err:
+                return Popen(["/opt/ccf/bin/sandbox.sh", "-p", "build/libccf_kvs.virtual.so", "--http2"], stdout=out, stderr=err)
 
     def bench(self, bench_cmd:List[str]):
-        p = Popen(CCF_KVS_BENCH_CMD+bench_cmd, stdout=sys.stdout, stderr=sys.stderr)
-        p.wait()
+        with open(os.path.join(BENCH_DIR, "ccf_kvs_bench.out"), 'w') as out:
+            with open(os.path.join(BENCH_DIR, "ccf_kvs_bench.err"), 'w') as err:
+                p = Popen(CCF_KVS_BENCH_CMD+bench_cmd, stdout=out, stderr=err)
+                p.wait()
 
 def run_benchmark(store, bench_cmd:List[str]):
     proc = store.spawn()
@@ -65,6 +76,9 @@ def run_benchmark(store, bench_cmd:List[str]):
     proc.wait()
 
 def main():
+    # make the bench directory
+    os.mkdir(BENCH_DIR)
+
     bench_cmd = ["put"]
     run_benchmark(EtcdStore(), bench_cmd)
 
