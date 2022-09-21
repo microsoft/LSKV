ba24e0e34d7f680c411d35d6df177afa1a6178e5
Andrew Jeffery <dev@jeffas.io>
2022-09-21 22:32:52 +0100

    Use logging

--- benchmark.py
+++ benchmark.py
@@ -3,17 +3,29 @@
 # Licensed under the MIT License.
 
 from subprocess import Popen
-import sys
 import time
+import logging
 import socket
 import os
 from typing import List
+
+logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=logging.INFO)
 
 ETCD = "etcd"
 CCF_KVS = "ccf_kvs"
 
 ETCD_BENCH_CMD = ["bin/benchmark", "--endpoints", "http://127.0.0.1:8000"]
-CCF_KVS_BENCH_CMD = ["bin/benchmark", "--endpoints", "https://127.0.0.1:8000", "--cacert", "workspace/sandbox_common/service_cert.pem", "--cert", "workspace/sandbox_common/user0_cert.pem", "--key", "workspace/sandbox_common/user0_privk.pem"]
+CCF_KVS_BENCH_CMD = [
+    "bin/benchmark",
+    "--endpoints",
+    "https://127.0.0.1:8000",
+    "--cacert",
+    "workspace/sandbox_common/service_cert.pem",
+    "--cert",
+    "workspace/sandbox_common/user0_cert.pem",
+    "--key",
+    "workspace/sandbox_common/user0_privk.pem",
+]
 
 
 def wait_for_port(port=8000):
@@ -28,64 +40,88 @@ def wait_for_port(port=8000):
             time.sleep(1)
     time.sleep(1)
 
+
 class Store:
-    def __init__(self, bench_dir:str):
+    def __init__(self, bench_dir: str):
         self.bench_dir = bench_dir
 
-    def spawn(self)->Popen:
+    def spawn(self) -> Popen:
         raise Exception("unimplemented")
 
-    def bench(self, _bench_cmd:List[str]):
+    def bench(self, _bench_cmd: List[str]):
         raise Exception("unimplemented")
 
     def name(self):
         raise Exception("unimplemented")
 
-class EtcdStore(Store):
-    def spawn(self)->Popen:
-        print(f"spawning {self.name()}")
-        client_urls = "http://127.0.0.1:8000"
-        with open(os.path.join(self.bench_dir, "etcd.out"), 'w') as out:
-            with open(os.path.join(self.bench_dir, "etcd.err"), 'w') as err:
-                return Popen(["bin/etcd", "--listen-client-urls", client_urls, "--advertise-client-urls", client_urls], stdout=out, stderr=err)
 
-    def bench(self, bench_cmd:List[str]):
-        with open(os.path.join(self.bench_dir, "etcd_bench.out"), 'w') as out:
-            with open(os.path.join(self.bench_dir, "etcd_bench.err"), 'w') as err:
-                p = Popen(ETCD_BENCH_CMD+bench_cmd, stdout=out, stderr=err)
+class EtcdStore(Store):
+    def spawn(self) -> Popen:
+        logging.info(f"spawning {self.name()}")
+        client_urls = "http://127.0.0.1:8000"
+        with open(os.path.join(self.bench_dir, "etcd.out"), "w") as out:
+            with open(os.path.join(self.bench_dir, "etcd.err"), "w") as err:
+                return Popen(
+                    [
+                        "bin/etcd",
+                        "--listen-client-urls",
+                        client_urls,
+                        "--advertise-client-urls",
+                        client_urls,
+                    ],
+                    stdout=out,
+                    stderr=err,
+                )
+
+    def bench(self, bench_cmd: List[str]):
+        with open(os.path.join(self.bench_dir, "etcd_bench.out"), "w") as out:
+            with open(os.path.join(self.bench_dir, "etcd_bench.err"), "w") as err:
+                p = Popen(ETCD_BENCH_CMD + bench_cmd, stdout=out, stderr=err)
                 p.wait()
 
-    def name(self)->str:
+    def name(self) -> str:
         return "etcd"
 
-class CCFKVSStore(Store):
-    def spawn(self)->Popen:
-        print(f"spawning {self.name()}")
-        with open(os.path.join(self.bench_dir, "ccf_kvs.out"), 'w') as out:
-            with open(os.path.join(self.bench_dir, "ccf_kvs.err"), 'w') as err:
-                return Popen(["/opt/ccf/bin/sandbox.sh", "-p", "build/libccf_kvs.virtual.so", "--http2"], stdout=out, stderr=err)
 
-    def bench(self, bench_cmd:List[str]):
-        with open(os.path.join(self.bench_dir, "ccf_kvs_bench.out"), 'w') as out:
-            with open(os.path.join(self.bench_dir, "ccf_kvs_bench.err"), 'w') as err:
-                p = Popen(CCF_KVS_BENCH_CMD+bench_cmd, stdout=out, stderr=err)
+class CCFKVSStore(Store):
+    def spawn(self) -> Popen:
+        logging.info(f"spawning {self.name()}")
+        with open(os.path.join(self.bench_dir, "ccf_kvs.out"), "w") as out:
+            with open(os.path.join(self.bench_dir, "ccf_kvs.err"), "w") as err:
+                return Popen(
+                    [
+                        "/opt/ccf/bin/sandbox.sh",
+                        "-p",
+                        "build/libccf_kvs.virtual.so",
+                        "--http2",
+                    ],
+                    stdout=out,
+                    stderr=err,
+                )
+
+    def bench(self, bench_cmd: List[str]):
+        with open(os.path.join(self.bench_dir, "ccf_kvs_bench.out"), "w") as out:
+            with open(os.path.join(self.bench_dir, "ccf_kvs_bench.err"), "w") as err:
+                p = Popen(CCF_KVS_BENCH_CMD + bench_cmd, stdout=out, stderr=err)
                 p.wait()
 
-    def name(self)->str:
+    def name(self) -> str:
         return "ccf_kvs"
 
-def run_benchmark(store, bench_cmd:List[str]):
+
+def run_benchmark(store, bench_cmd: List[str]):
     proc = store.spawn()
 
     wait_for_port()
 
-    print(f"starting benchmark for {store.name()}")
+    logging.info(f"starting benchmark for {store.name()}")
     store.bench(bench_cmd)
-    print(f"stopping benchmark for {store.name()}")
+    logging.info(f"stopping benchmark for {store.name()}")
 
     proc.terminate()
     proc.wait()
-    print(f"stopped {store.name()}")
+    logging.info(f"stopped {store.name()}")
+
 
 def main():
     bench_dir = "bench"
@@ -93,9 +129,10 @@ def main():
     if not os.path.exists(bench_dir):
         os.mkdir(bench_dir)
 
+    # todo: write a kv into the store for the range query benchmark
     bench_cmds = [["put"], ["range", "key"]]
     for bench_cmd in bench_cmds:
-        print(f"benching with extra args {bench_cmd}")
+        logging.info(f"benching with extra args {bench_cmd}")
 
         d = os.path.join(bench_dir, "_".join(bench_cmd))
         if not os.path.exists(d):
@@ -104,6 +141,7 @@ def main():
         run_benchmark(EtcdStore(d), bench_cmd)
 
         run_benchmark(CCFKVSStore(d), bench_cmd)
+
 
 if __name__ == "__main__":
     main()
