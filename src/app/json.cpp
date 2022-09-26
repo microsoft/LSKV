198a3529f5890ae41ee76a04d90717081b476e6e
Andrew Jeffery <dev@jeffas.io>
2022-09-26 17:27:16 +0100

    Add convenience macro

--- src/app/json.cpp
+++ src/app/json.cpp
@@ -5,9 +5,18 @@
 
 #include "nlohmann/json.hpp"
 
+#define CONTAINS_THEN_SET(ty, var) \
+  if (j.contains(#var)) \
+  { \
+    ty v; \
+    j.at(#var).get_to(v); \
+    req.set_##var(v); \
+  }
+
 namespace etcdserverpb
 {
   using json = nlohmann::json;
+
   void from_json(const json& j, RangeRequest& req)
   {
     j.at("key").get_to(*req.mutable_key());
@@ -16,71 +25,18 @@ namespace etcdserverpb
       j.at("range_end").get_to(*req.mutable_range_end());
     }
 
-    if (j.contains("limit"))
-    {
-      int64_t limit;
-      j.at("limit").get_to(limit);
-      req.set_limit(limit);
-    }
-
-    if (j.contains("revision"))
-    {
-      int64_t revision;
-      j.at("revision").get_to(revision);
-      req.set_revision(revision);
-    }
-
-    if (j.contains("serializable"))
-    {
-      bool serializable;
-      j.at("serializable").get_to(serializable);
-      req.set_serializable(serializable);
-    }
+    CONTAINS_THEN_SET(int64_t, limit);
+    CONTAINS_THEN_SET(int64_t, revision);
+    CONTAINS_THEN_SET(bool, serializable);
+    CONTAINS_THEN_SET(bool, keys_only);
+    CONTAINS_THEN_SET(bool, count_only);
+    CONTAINS_THEN_SET(int64_t, min_mod_revision);
+    CONTAINS_THEN_SET(int64_t, max_mod_revision);
+    CONTAINS_THEN_SET(int64_t, min_create_revision);
+    CONTAINS_THEN_SET(int64_t, max_create_revision);
 
     // sort_order
     // sort_target
-
-    if (j.contains("keys_only"))
-    {
-      bool keys_only;
-      j.at("keys_only").get_to(keys_only);
-      req.set_keys_only(keys_only);
-    }
-
-    if (j.contains("count_only"))
-    {
-      bool count_only;
-      j.at("count_only").get_to(count_only);
-      req.set_count_only(count_only);
-    }
-
-    if (j.contains("min_mod_revision"))
-    {
-      int64_t min_mod_revision;
-      j.at("min_mod_revision").get_to(min_mod_revision);
-      req.set_min_mod_revision(min_mod_revision);
-    }
-
-    if (j.contains("max_mod_revision"))
-    {
-      int64_t max_mod_revision;
-      j.at("max_mod_revision").get_to(max_mod_revision);
-      req.set_max_mod_revision(max_mod_revision);
-    }
-
-    if (j.contains("min_create_revision"))
-    {
-      int64_t min_create_revision;
-      j.at("min_create_revision").get_to(min_create_revision);
-      req.set_min_create_revision(min_create_revision);
-    }
-
-    if (j.contains("max_create_revision"))
-    {
-      int64_t max_create_revision;
-      j.at("max_create_revision").get_to(max_create_revision);
-      req.set_max_create_revision(max_create_revision);
-    }
   }
 
   void to_json(json& j, const KeyValue& kv)
@@ -112,33 +68,10 @@ namespace etcdserverpb
     j.at("key").get_to(*req.mutable_key());
     j.at("value").get_to(*req.mutable_value());
 
-    if (j.contains("lease"))
-    {
-      int64_t lease;
-      j.at("lease").get_to(lease);
-      req.set_lease(lease);
-    }
-
-    if (j.contains("prev_kv"))
-    {
-      bool prev_kv;
-      j.at("prev_kv").get_to(prev_kv);
-      req.set_prev_kv(prev_kv);
-    }
-
-    if (j.contains("ignore_value"))
-    {
-      bool ignore_value;
-      j.at("ignore_value").get_to(ignore_value);
-      req.set_ignore_value(ignore_value);
-    }
-
-    if (j.contains("ignore_lease"))
-    {
-      bool ignore_lease;
-      j.at("ignore_lease").get_to(ignore_lease);
-      req.set_ignore_lease(ignore_lease);
-    }
+    CONTAINS_THEN_SET(int64_t, lease);
+    CONTAINS_THEN_SET(bool, prev_kv);
+    CONTAINS_THEN_SET(bool, ignore_value);
+    CONTAINS_THEN_SET(bool, ignore_lease);
   }
 
   void to_json(json& j, const PutResponse& res)
@@ -157,12 +90,7 @@ namespace etcdserverpb
       j.at("range_end").get_to(*req.mutable_range_end());
     }
 
-    if (j.contains("prev_kv"))
-    {
-      bool prev_kv;
-      j.at("prev_kv").get_to(prev_kv);
-      req.set_prev_kv(prev_kv);
-    }
+    CONTAINS_THEN_SET(bool, prev_kv);
   }
 
   void to_json(json& j, const DeleteRangeResponse& res)
