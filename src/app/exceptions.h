79e2a483d1af7c637408d75d9d83c234d59b0ba7
Andrew Jeffery <dev@jeffas.io>
2022-09-28 11:44:57 +0100

    Add license header and fix formatting

--- src/app/exceptions.h
+++ src/app/exceptions.h
@@ -1,8 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 #pragma once
 
 namespace app::exceptions
 {
-
   struct BadRequest : public std::exception
   {
     ccf::ErrorDetails error;
