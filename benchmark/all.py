8ffdbc532af05c17a09d338becbaa4e1cf323fc4
Andrew Jeffery <dev@jeffas.io>
2022-10-21 14:10:21 +0000

    Add perf_system to all benchmarks

--- benchmark/all.py
+++ benchmark/all.py
@@ -13,6 +13,7 @@ from typing import List
 import common
 import etcd
 import ycsb
+import perf_system
 
 logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
 
@@ -60,11 +61,24 @@ def all_ycsb_configurations(args: argparse.Namespace) -> List[ycsb.YCSBConfig]:
     all_common_configurations(args)
     return ycsb.make_configurations(args)
 
+def all_perf_configurations(args: argparse.Namespace) -> List[perf_system.PerfConfig]:
+    """
+    Set args for all perf configurations.
+    """
+    args.http1=True
+    args.http2=True
+
+    all_common_configurations(args)
+    return perf_system.make_configurations(args)
+
 
 if __name__ == "__main__":
+    # common.main(
+    #     "etcd", etcd.get_arguments, all_etcd_configurations, etcd.execute_config
+    # )
+    # common.main(
+    #     "ycsb", ycsb.get_arguments, all_ycsb_configurations, ycsb.execute_config
+    # )
     common.main(
-        "etcd", etcd.get_arguments, all_etcd_configurations, etcd.execute_config
-    )
-    common.main(
-        "ycsb", ycsb.get_arguments, all_ycsb_configurations, ycsb.execute_config
+        "perf", perf_system.get_arguments, all_perf_configurations, perf_system.execute_config
     )
