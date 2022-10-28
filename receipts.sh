8366ec613e4d36da26c71263641e2925d135a5d9
Andrew Jeffery <dev@jeffas.io>
2022-10-28 10:54:59 +0000

    Add receipts test script

---
+++ receipts.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+header=$(./etcdctl.sh put a b -w json | jq '.header')
+rev=$(echo $header | jq '.revision')
+raft_term=$(echo $header | jq '.raft_term')
+
+curl -X POST -k https://127.0.0.1:8000/v3/receipt/get_receipt -d '{"revision": "'$rev'", "raft_term": "'$raft_term'"}' -H 'content-type: application/json'
+curl -X POST -k https://127.0.0.1:8000/v3/receipt/get_receipt -d '{"revision": "'$rev'", "raft_term": "'$raft_term'"}' -H 'content-type: application/json' | jq
+
+sig_rev=$((rev+1))
+curl -X POST -k https://127.0.0.1:8000/v3/receipt/get_receipt -d '{"revision": "'$sig_rev'", "raft_term": "'$raft_term'"}' -H 'content-type: application/json' 
+curl -X POST -k https://127.0.0.1:8000/v3/receipt/get_receipt -d '{"revision": "'$sig_rev'", "raft_term": "'$raft_term'"}' -H 'content-type: application/json' | jq
