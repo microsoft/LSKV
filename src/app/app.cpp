5e8de9d114f96a75779041b7cb18f0134da4c47f
Andrew Jeffery <dev@jeffas.io>
2022-11-09 09:56:45 +0000

    Set version

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -38,8 +38,6 @@
     ctx.rpc_ctx->set_claims_digest(ccf::ClaimsDigest::Digest(claims_data)); \
   }
 
-const VERSION = "0.1.0";
-
 namespace app
 {
   class AppHandlers : public ccf::UserEndpointRegistry
@@ -1193,7 +1191,7 @@ namespace app
       CCF_APP_DEBUG("MAINTENANCE STATUS");
 
       response.set_leader(0);
-      response.set_version(VERSION);
+      response.set_version(LSKV_VERSION);
 
       return ccf::grpc::make_success(response);
     }
