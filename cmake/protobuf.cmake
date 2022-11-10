c947bb1863aaa4cd4613ba75cb80db2cb43379cd
Andrew Jeffery <dev@jeffas.io>
2022-11-10 14:22:31 +0000

    Fix outputs from python grpc

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -20,9 +20,11 @@ function(build_proto proto_file src_dir)
   if(${BUILD_TESTING})
     add_custom_command(
       OUTPUT ${CMAKE_SOURCE_DIR}/tests/${PROTO_DIR}/${PROTO_NAME_WE}_pb2.py
+             ${CMAKE_SOURCE_DIR}/tests/${PROTO_DIR}/${PROTO_NAME_WE}_pb2.pyi
+             ${CMAKE_SOURCE_DIR}/tests/${PROTO_DIR}/${PROTO_NAME_WE}_pb2_grpc.py
       COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/build.sh ${src_dir}/${proto_file}
               ${CMAKE_SOURCE_DIR}/tests/
-      COMMENT "Generate Python source file from protobuf file ${PROTO_NAME}"
+      COMMENT "Generate Python source files from protobuf file ${PROTO_NAME}"
       DEPENDS ${src_dir}/${proto_file} ${CMAKE_CURRENT_SOURCE_DIR}/build.sh)
     add_custom_target(
       ${PROTO_NAME_WE}_proto_python ALL
