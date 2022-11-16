c765b3879ebb4c7b6e4d1911bf4aad0110c5c2a4
Andrew Jeffery <dev@jeffas.io>
2022-11-16 18:47:43 +0000

    Use full name for package arg

--- nix/lskv-sandbox.nix
+++ nix/lskv-sandbox.nix
@@ -11,5 +11,5 @@
   sandbox = ccf-sandbox {inherit enclave;};
 in
   writeShellScriptBin "lskv-sandbox.sh" ''
-    ${sandbox}/bin/sandbox.sh -p ${l}/lib/liblskv --enclave-type ${enclave_type} $@
+    ${sandbox}/bin/sandbox.sh --package ${l}/lib/liblskv --enclave-type ${enclave_type} $@
   ''
