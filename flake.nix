2d61ae47198792f6a267d28e2d33e14945c10182
Andrew Jeffery <dev@jeffas.io>
2022-11-25 14:00:35 +0000

    Fix overlay

--- flake.nix
+++ flake.nix
@@ -23,13 +23,13 @@
     packages.${system} =
       flake-utils.lib.filterPackages system nix;
 
-    overlays.${system}.default = final: prev:
-      (nix.lib.forAllPlatforms {
+    overlays.default = final: prev:
+      ((nix.lib.forAllPlatforms {
         inherit (self.packages.${system}) ccf ccf-sandbox lskv lskv-sandbox;
       })
       // {
         inherit (self.packages.${system}) openenclave az-dcap sgx-dcap;
-      };
+      });
 
     checks.${system} =
       pkgs.lib.attrsets.filterAttrs
