b576618420a4e957f744c5cd4e859e19ac96ab68
Andrew Jeffery <dev@jeffas.io>
2022-11-28 13:13:02 +0000

    Remove extra enclaves only for sgx platform

--- nix/ccf.nix
+++ nix/ccf.nix
@@ -13,54 +13,61 @@
   openssl,
   arrow-cpp,
   platform ? "virtual",
-}:
-stdenv.mkDerivation rec {
-  pname = "ccf-${platform}";
-  version = "4.0.0-dev0";
-  src = fetchFromGitHub {
-    owner = "microsoft";
-    repo = "CCF";
-    name = "ccf-${version}";
-    rev = "ccf-${version}";
-    hash = "sha256-pnopMvATNoQxEJ68wA+A6eLqFVnxBHkdOUNcJXi+Abs=";
-  };
-  patches = [
-    patches/ccf-no-python.diff
-    patches/ccf-no-python-pb2.diff
-    patches/ccf-protoc-binary.diff
-    patches/ccf-ignore-submitter.diff
-  ];
+}: let
+  toRemove =
+    if platform == "sgx"
+    then ''
+      # These are signed with a randomly generated key, which makes the build non-reproducible
+      rm $out/lib/libjs_generic.enclave.so.debuggable \
+         $out/lib/libjs_generic.enclave.so.signed
+    ''
+    else "";
+in
+  stdenv.mkDerivation rec {
+    pname = "ccf-${platform}";
+    version = "4.0.0-dev0";
+    src = fetchFromGitHub {
+      owner = "microsoft";
+      repo = "CCF";
+      name = "ccf-${version}";
+      rev = "ccf-${version}";
+      hash = "sha256-pnopMvATNoQxEJ68wA+A6eLqFVnxBHkdOUNcJXi+Abs=";
+    };
+    patches = [
+      patches/ccf-no-python.diff
+      patches/ccf-no-python-pb2.diff
+      patches/ccf-protoc-binary.diff
+      patches/ccf-ignore-submitter.diff
+    ];
 
-  nativeBuildInputs = [
-    cmake
-    ninja
-    libuv
-    protobuf
-    arrow-cpp
-    sgx-dcap
-    openenclave
-    makeWrapper
-  ];
+    nativeBuildInputs = [
+      cmake
+      ninja
+      libuv
+      protobuf
+      arrow-cpp
+      sgx-dcap
+      openenclave
+      makeWrapper
+    ];
 
-  cmakeFlags = [
-    "-DBUILD_TESTS=OFF"
-    "-DBUILD_UNIT_TESTS=OFF"
-    "-DLVI_MITIGATIONS=OFF"
-    "-DCOMPILE_TARGET=${platform}"
-  ];
+    cmakeFlags = [
+      "-DBUILD_TESTS=OFF"
+      "-DBUILD_UNIT_TESTS=OFF"
+      "-DLVI_MITIGATIONS=OFF"
+      "-DCOMPILE_TARGET=${platform}"
+    ];
 
-  NIX_CFLAGS_COMPILE = "-Wno-unused-command-line-argument";
-  NIX_NO_SELF_RPATH = "1";
+    NIX_CFLAGS_COMPILE = "-Wno-unused-command-line-argument";
+    NIX_NO_SELF_RPATH = "1";
 
-  postInstall = ''
-    wrapProgram $out/bin/cchost \
-      --suffix LD_LIBRARY_PATH ':' "${az-dcap}/lib:${sgx-psw}/lib:${sgx-dcap}/lib"
+    postInstall = ''
+      wrapProgram $out/bin/cchost \
+        --suffix LD_LIBRARY_PATH ':' "${az-dcap}/lib:${sgx-psw}/lib:${sgx-dcap}/lib"
 
-    wrapProgram $out/bin/keygenerator.sh \
-      --prefix PATH ':' "${openssl}/bin"
+      wrapProgram $out/bin/keygenerator.sh \
+        --prefix PATH ':' "${openssl}/bin"
 
-    # These are signed with a randomly generated key, which makes the build non-reproducible
-    rm -f $out/lib/libjs_generic.enclave.so.debuggable \
-       $out/lib/libjs_generic.enclave.so.signed
-  '';
-}
+      ${toRemove}
+    '';
+  }
