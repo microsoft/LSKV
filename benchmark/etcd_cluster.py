14516f638fa03e339f9f3f345d3c0a90bf1cd45c
Andrew Jeffery <dev@jeffas.io>
2023-01-05 12:49:56 +0000

    Run each etcd node from directory

--- benchmark/etcd_cluster.py
+++ benchmark/etcd_cluster.py
@@ -14,7 +14,6 @@ import certs
 import shutil
 import signal
 import subprocess
-import tempfile
 
 from loguru import logger
 
@@ -26,7 +25,6 @@ def spawn_node(
     scheme: str,
     address: str,
     port: int,
-    data_dir: str,
     initial_cluster: str,
 ) -> subprocess.Popen:
     """
@@ -37,10 +35,37 @@ def spawn_node(
     metrics_port = client_port + 2
 
     name = f"node{node_index}"
+
+    node_dir = os.path.join(workspace, name)
+    os.makedirs(node_dir)
+
+    # copy binary files to node dir
+    for file in ["bin/etcd"]:
+        src = os.path.abspath(file)
+        dst = os.path.join(node_dir, os.path.basename(file))
+        logger.info("Copying binary file from {} to {}", src, dst)
+        shutil.copy(src, dst)
+
+    ca_cert = "ca.pem"
+    server_cert = "server.pem"
+    server_key = "server-key.pem"
+    peer_cert = f"{name}.pem"
+    peer_key = f"{name}-key.pem"
+
+    # copy data files to node dir
+    for file in [
+        os.path.join("certs", n)
+        for n in [ca_cert, server_cert, server_key, peer_cert, peer_key]
+    ]:
+        src = os.path.join(workspace, file)
+        dst = os.path.join(node_dir, os.path.basename(file))
+        logger.info("Copying data file from {} to {}", src, dst)
+        shutil.copy(src, dst)
+
     cmd = [
-        "bin/etcd",
+        "./etcd",
         "--data-dir",
-        data_dir,
+        "data",
         "--listen-client-urls",
         f"{scheme}://{address}:{client_port}",
         "--advertise-client-urls",
@@ -60,27 +85,29 @@ def spawn_node(
     ]
 
     if scheme == "https":
-        certs_dir = os.path.join(workspace, "certs")
         cmd += [
             "--trusted-ca-file",
-            os.path.join(certs_dir, "ca.pem"),
+            ca_cert,
             "--cert-file",
-            os.path.join(certs_dir, "server.pem"),
+            server_cert,
             "--key-file",
-            os.path.join(certs_dir, f"server-key.pem"),
+            server_key,
             "--client-cert-auth",
             "--peer-trusted-ca-file",
-            os.path.join(certs_dir, "ca.pem"),
+            ca_cert,
             "--peer-cert-file",
-            os.path.join(certs_dir, f"{name}.pem"),
+            peer_cert,
             "--peer-key-file",
-            os.path.join(certs_dir, f"{name}-key.pem"),
+            peer_key,
             "--peer-client-cert-auth",
         ]
 
     logger.info("running etcd node: {}", " ".join(cmd))
 
-    return subprocess.Popen(cmd)
+    out = open(os.path.join(node_dir, "out"), "w")
+    err = open(os.path.join(node_dir, "err"), "w")
+
+    return subprocess.Popen(cmd, stdout=out, stderr=err, cwd=node_dir)
 
 
 def generate_certs(workspace: str, nodes: List[Tuple[str, int]]):
@@ -144,7 +171,9 @@ def main():
     else:
         parser.error("Found unexpected prefix")
 
-    node_addresses = [(n[1].split(":")[0], int(n[1].split(":")[1])) for n in node_addresses_full]
+    node_addresses = [
+        (n[1].split(":")[0], int(n[1].split(":")[1])) for n in node_addresses_full
+    ]
     logger.info("Made addresses {}", node_addresses)
 
     if args.scheme == "https":
@@ -159,13 +188,10 @@ def main():
     logger.info("Built initial cluster {}", initial_cluster)
 
     processes = []
-    data_dirs = []
 
     try:
         for i, (ip, port) in enumerate(node_addresses):
             logger.info("spawning node {}: {}", i, (ip, port))
-            data_dir = tempfile.mkdtemp()
-            data_dirs.append(data_dir)
             processes.append(
                 spawn_node(
                     i,
@@ -173,7 +199,6 @@ def main():
                     args.scheme,
                     ip,
                     port,
-                    data_dir,
                     initial_cluster,
                 )
             )
@@ -195,10 +220,6 @@ def main():
             process.kill()
             logger.info("waiting for process {}", i)
             process.wait()
-
-        for i, data_dir in enumerate(data_dirs):
-            logger.info("removing data dir {}: {}", i, data_dir)
-            shutil.rmtree(data_dir)
 
         logger.info("all processes finished")
 
