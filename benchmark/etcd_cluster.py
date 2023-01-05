791ae42c0b1a82368fb51080ff52e99b137d9620
Andrew Jeffery <dev@jeffas.io>
2023-01-05 12:10:50 +0000

    Generate certs in etcd workspace

--- benchmark/etcd_cluster.py
+++ benchmark/etcd_cluster.py
@@ -7,6 +7,10 @@ Setup and run a local etcd cluster.
 """
 
 import argparse
+import os
+import os.path
+from typing import List, Tuple
+import certs
 import shutil
 import signal
 import subprocess
@@ -18,17 +22,12 @@ from loguru import logger
 # pylint: disable=too-many-arguments
 def spawn_node(
     node_index: int,
+    workspace: str,
     scheme: str,
     address: str,
     port: int,
     data_dir: str,
     initial_cluster: str,
-    cacert: str,
-    cert: str,
-    key: str,
-    peer_cacert: str,
-    peer_cert: str,
-    peer_key: str,
 ) -> subprocess.Popen:
     """
     Spawn a new etcd node.
@@ -37,6 +36,7 @@ def spawn_node(
     peer_port = client_port + 1
     metrics_port = client_port + 2
 
+    name = f"node{node_index}"
     cmd = [
         "bin/etcd",
         "--data-dir",
@@ -56,29 +56,58 @@ def spawn_node(
         "--initial-cluster-state",
         "new",
         "--name",
-        f"node{node_index}",
+        name,
     ]
+
     if scheme == "https":
+        certs_dir = os.path.join(workspace, "certs")
         cmd += [
             "--trusted-ca-file",
-            cacert,
+            os.path.join(certs_dir, "ca.pem"),
             "--cert-file",
-            cert,
+            os.path.join(certs_dir, "server.pem"),
             "--key-file",
-            key,
+            os.path.join(certs_dir, f"server-key.pem"),
             "--client-cert-auth",
             "--peer-trusted-ca-file",
-            peer_cacert,
+            os.path.join(certs_dir, "ca.pem"),
             "--peer-cert-file",
-            peer_cert,
+            os.path.join(certs_dir, f"{name}.pem"),
             "--peer-key-file",
-            peer_key,
+            os.path.join(certs_dir, f"{name}-key.pem"),
             "--peer-client-cert-auth",
         ]
 
     logger.info("running etcd node: {}", " ".join(cmd))
 
     return subprocess.Popen(cmd)
+
+
+def generate_certs(workspace: str, nodes: List[Tuple[str, int]]):
+    """
+    Generate certs to be used for the cluster.
+    """
+    certs_dir = os.path.join(workspace, "certs")
+    os.makedirs(certs_dir)
+    # cacert
+    cfssl = os.path.abspath("bin/cfssl")
+    cfssljson = os.path.abspath("bin/cfssljson")
+    logger.info("Using cfssl {}", cfssl)
+    logger.info("Using cfssljson {}", cfssljson)
+    certs.make_ca(certs_dir, cfssl, cfssljson)
+
+    certs.make_certs(certs_dir, cfssl, cfssljson, "server", "server", certs.SERVER_CSR)
+
+    for i, (ip, _port) in enumerate(nodes):
+        peer_csr = certs.PEER_CSR
+        name = f"node{i}"
+        peer_csr["CN"] = name
+        if ip not in peer_csr["hosts"]:
+            peer_csr["hosts"].append(ip)
+
+        certs.make_certs(certs_dir, cfssl, cfssljson, "peer", name, peer_csr)
+
+    certs.make_certs(certs_dir, cfssl, cfssljson, "client", "client", certs.CLIENT_CSR)
 
 
 def main():
@@ -88,21 +117,27 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--node", action="extend", nargs="+", type=str)
     parser.add_argument("--scheme", type=str, default="https")
-    parser.add_argument("--cacert", type=str, default="certs/ca.pem")
-    parser.add_argument("--cert", type=str, default="certs/server.pem")
-    parser.add_argument("--key", type=str, default="certs/server-key.pem")
-    parser.add_argument("--peer-cacert", type=str, default="certs/ca.pem")
-    parser.add_argument("--peer-cert-base", type=str, default="certs/node")
+    parser.add_argument("--workspace", type=str, default="workspace")
     args = parser.parse_args()
 
     if not args.node:
         parser.error("must have at least one node to run")
 
+    args.workspace = os.path.abspath(args.workspace)
+
     logger.info("using config {}", args)
 
+    if os.path.exists(args.workspace):
+        logger.info("Removing existing workspace dir: {}", args.workspace)
+        shutil.rmtree(args.workspace)
+    os.makedirs(args.workspace)
+
     node_addresses = [n.split("://")[1] for n in args.node]
-    node_addresses = [(n.split(":")[0], int(n.split(":")[1]) )for n in node_addresses]
+    node_addresses = [(n.split(":")[0], int(n.split(":")[1])) for n in node_addresses]
     logger.info("Made addresses {}", node_addresses)
+
+    if args.scheme == "https":
+        generate_certs(args.workspace, node_addresses)
 
     initial_cluster = ",".join(
         [
@@ -120,22 +155,15 @@ def main():
             logger.info("spawning node {}: {}", i, (ip, port))
             data_dir = tempfile.mkdtemp()
             data_dirs.append(data_dir)
-            peer_cert = f"{args.peer_cert_base}{i}.pem"
-            peer_key = f"{args.peer_cert_base}{i}-key.pem"
             processes.append(
                 spawn_node(
                     i,
+                    args.workspace,
                     args.scheme,
                     ip,
                     port,
                     data_dir,
                     initial_cluster,
-                    args.cacert,
-                    args.cert,
-                    args.key,
-                    args.peer_cacert,
-                    peer_cert,
-                    peer_key,
                 )
             )
             logger.info("spawned node {}: {}", i, (ip, port))
