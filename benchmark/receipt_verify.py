f57a43962b40b1de2ba11dacfa8069a95b9b1797
Andrew Jeffery <dev@jeffas.io>
2023-01-10 11:17:28 +0000

    Check node cert is endorsed by service in receipt verify benchmark

--- benchmark/receipt_verify.py
+++ benchmark/receipt_verify.py
@@ -6,16 +6,15 @@ import lskvserver_pb2  # type: ignore
 import json
 import timeit
 import hashlib
-from loguru import logger
-from google.protobuf.json_format import MessageToDict, ParseDict
+from google.protobuf.json_format import ParseDict
 
 
-def check_receipt(req_type: str, request, response, receipt, receipt_json):
+def check_receipt(
+    req_type: str, request, response, receipt, receipt_json, service_cert
+):
     """
     Check a receipt for a request and response.
     """
-    rev, term = response.header.revision, response.header.raft_term
-
     receipt = receipt.receipt
     tx_receipt = receipt.tx_receipt
     leaf_components = tx_receipt.leaf_components
@@ -51,8 +50,10 @@ def check_receipt(req_type: str, request, response, receipt, receipt_json):
     claims_digest_calculated = hashlib.sha256(claims_ser).hexdigest()
     assert claims_digest == claims_digest_calculated
 
+    ccf.receipt.check_endorsement(node_cert, service_cert)
 
-def check_json_receipt(req_type, req, res, receipt):
+
+def check_json_receipt(req_type, req, res, receipt, service_cert):
     req = json.loads(req)
     req_pb = ParseDict(req, etcd_pb2.PutRequest())
     res = json.loads(res)
@@ -60,7 +61,7 @@ def check_json_receipt(req_type, req, res, receipt):
     receipt = json.loads(receipt)
     receipt_pb = ParseDict(receipt, lskvserver_pb2.GetReceiptResponse())
 
-    check_receipt(req_type, req_pb, res_pb, receipt_pb, receipt)
+    check_receipt(req_type, req_pb, res_pb, receipt_pb, receipt, service_cert)
 
 
 def main():
@@ -68,21 +69,43 @@ def main():
     repeats = 10
 
     req_type = "put"
-    req = '{"key":"YQ==","value":"Yg=="}'
+    req = '{"key":"Zm9v","value":"YmFy"}'
     res = '{"header":{"cluster_id":1201806628430307423,"member_id":15247274768972111916,"revision":11,"raft_term":2}}'
-    receipt = '{"header":{"clusterId":"1201806628430307423","memberId":"15247274768972111916","revision":"12","raftTerm":"2","committedRevision":"12","committedRaftTerm":"2"},"receipt":{"cert":"-----BEGIN CERTIFICATE-----\\nMIIBwzCCAUmgAwIBAgIRAKYlYuAhGIfcih3QDJsVfH4wCgYIKoZIzj0EAwMwFjEU\\nMBIGA1UEAwwLQ0NGIE5ldHdvcmswHhcNMjMwMTA2MTIwODM2WhcNMjMwNDA2MTIw\\nODM1WjATMREwDwYDVQQDDAhDQ0YgTm9kZTB2MBAGByqGSM49AgEGBSuBBAAiA2IA\\nBB0era6qeg/KkKhxvfJUI3bwSYToBuuAxiB1O4H/YbVoq+/j9ljf6SAPA/lEMwaB\\nUaq3Tf2GGhjIwBFQsiD7ZDpC8+UhDxzGRNrCa8kUqAvXdr2AZoGc3Bpiy812qQDL\\ncaNeMFwwCQYDVR0TBAIwADAdBgNVHQ4EFgQUZVBGS+4onlJn1H0mEQL0eOIC6U4w\\nHwYDVR0jBBgwFoAUF6/StYa1PdeEITb16OPG/LcoFl8wDwYDVR0RBAgwBocEfwAA\\nATAKBggqhkjOPQQDAwNoADBlAjEAolzbk5mSRdJ2kat0FzJgy9jzTe38BDFFZYl2\\nlhLN8CFfWkfWsEGU12ZO4HUfZz5xAjApJfoOAUg13hIpseGjFCOFVD8QlGVZVjz4\\nu91XTau+62uMBoGfLG6QDxmEjYWHzCQ=\\n-----END CERTIFICATE-----\\n","signature":"MGQCME8JI0fJqsJp0Asor/C3dQjt/b5xSQs6LF1pM7MZikPV3VSewct8Ll5POz4OqHg1UwIwQbIo9hdZjA4h1hcWUV+8OhobsnaDvAhRQ68rYb1fovt28bx/rm2LsPPzeyzbFTLq","nodeId":"2ce05b6d9e3399d3a38a740c9bb82114f75fab3c493069186767a903d4ca9cf6","txReceipt":{"leafComponents":{"claimsDigest":"7efbc55203a0e1ea810e2e8c1a9561ff650ac0d5de623b3f35f145823466edc9","commitEvidence":"ce:2.11:166dae27360e38b6657b90dca456c5ab2b805bc008a3892b274b7d2b2bb72c8a","writeSetDigest":"4d67287b7a5b6b808e9cbb84017e27a57d83d60847def8ccffe273f82c6d6a2d"},"proof":[{"left":"630a7b723504d69c95ae7ce64499f37060da2fa351f1b18d29e13f2695c5cbbb"},{"left":"3f7582f3b824d2af7a81bf106514a6c817332ea346a918c2fc45cb12c2c0cacc"},{"left":"822d9b9ead97bc353d3e044316636155650b25e1f15d819d5e07185786108d77"}]}}}'
+    receipt = '{"header":{"clusterId":"3170697659173810570","memberId":"17617643898592276624","revision":"12","raftTerm":"2","committedRevision":"12","committedRaftTerm":"2"},"receipt":{"cert":"-----BEGIN CERTIFICATE-----\\nMIIBwjCCAUigAwIBAgIQelWMyA4HX9YNRNHbjBB+GjAKBggqhkjOPQQDAzAWMRQw\\nEgYDVQQDDAtDQ0YgTmV0d29yazAeFw0yMzAxMTAxMTA4MDFaFw0yMzA0MTAxMTA4\\nMDBaMBMxETAPBgNVBAMMCENDRiBOb2RlMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAE\\nRcyck/p0JwM2vzGWxZwre3KF6HZWaC1pKDgBXCmOzLQ8FkOpiLiCdbiKum4wXZUB\\ncFRZXhBB0aFqPuLBlJrhF/EsDHZXw3q55cc1vJwQA6B8xqZpI5EeHBHzf/5nhVCt\\no14wXDAJBgNVHRMEAjAAMB0GA1UdDgQWBBSUkeYJQCUK2zRePcyb3ijK4joAzjAf\\nBgNVHSMEGDAWgBS70Mmt6puVhK8amGjMh9N8IHgPMDAPBgNVHREECDAGhwR/AAAB\\nMAoGCCqGSM49BAMDA2gAMGUCMQCr2zslxIrmzZua/6cVjWMMHTQsFWQQWqIEMpnN\\nt1QPC8H8kXmObnUBLnZ7xzPxLAICMAlui+wqJ2T781mlb9srf1ZB+LA4jUTCMa7/\\nRetTCm6wS4NSw2KODmt/awa11e6sUg==\\n-----END CERTIFICATE-----\\n","signature":"MGUCMB/QRsaoEmWwn5lf9E/7Suct4zsUCAoFr06wqL6mMd2+0VfH6WtfAGj0grSOjeheaAIxAPp8pKiQg8m/A9ykTq4JmkrNfgIjHpt5lx+tbBQWq7+v8EwwT2GJw6NdB/HOq1ibWA==","nodeId":"90b0eb92c8717ef4af94f2ad35dad1d0197c8dd9e9c46c2444cd0dced76780db","txReceipt":{"leafComponents":{"claimsDigest":"1e2ce1004f14e362833d5791b3d9702468d4a276665606349e9afb58b45365bd","commitEvidence":"ce:2.11:c296e4e2f3541ad12dd3cc7d90682f06a4ef6bb6fe78c97da227fccadb213ba9","writeSetDigest":"0dcbe265fd4844cf8e191df4edf8b359da18aa25d61a5d8927541f43b5e1851f"},"proof":[{"left":"6bc6a9ca7318c9c5464ad52a23761f2088090ae4632b33472b262e087bdf5e95"},{"left":"14ca017e27abc6e1e7713419c46b902b4d4926bc8639a92a916894ce83090a15"},{"left":"bf45d3693fe0bb96f73532dfe74bb1134a76c89bd6d113c4aaef3afa12e194d5"}]}}}'
+    service_cert = load_pem_x509_certificate(
+        """-----BEGIN CERTIFICATE-----
+MIIBuDCCAT6gAwIBAgIRALGT6aKJj03G0glrrTxVumAwCgYIKoZIzj0EAwMwFjEU
+MBIGA1UEAwwLQ0NGIE5ldHdvcmswHhcNMjMwMTEwMTEwODAxWhcNMjMwNDEwMTEw
+ODAwWjAWMRQwEgYDVQQDDAtDQ0YgTmV0d29yazB2MBAGByqGSM49AgEGBSuBBAAi
+A2IABBUSXLZ/qHxuDio17jtXUzo0fbi8x0+nbaYRMV1sam48OmMWzKqcXjiPPxht
+JlefGs4011X4btVFvK7sJtpC7nj36RdwPSh7dsozjlRKmJo73yeMreSq7DoIWILi
+De98G6NQME4wDAYDVR0TBAUwAwEB/zAdBgNVHQ4EFgQUu9DJreqblYSvGphozIfT
+fCB4DzAwHwYDVR0jBBgwFoAUu9DJreqblYSvGphozIfTfCB4DzAwCgYIKoZIzj0E
+AwMDaAAwZQIxAM2LxMnWaLDtGAMEMqaH03HaZV0CnY+s3uRkp7mCElcfMAFXY2vB
+CW1ZOzn7qIUvBgIwYDcYqeN8Ox9y9ktgpLEkvdiRK6OLIF4dxnsQJ/ORjSNLPyYx
+RuXUu+yl3EgtEgvw
+-----END CERTIFICATE-----""".encode()
+    )
 
-    print(f"Timing receipt verification with {iterations} iterations and {repeats} repeats")
+    print(
+        f"Timing receipt verification with {iterations} iterations and {repeats} repeats"
+    )
 
     durations = timeit.Timer(
-        lambda: check_json_receipt(req_type, req, res, receipt)
+        lambda: check_json_receipt(req_type, req, res, receipt, service_cert)
     ).repeat(number=iterations, repeat=repeats)
 
     for duration in durations:
-        print(f"Took {duration} seconds to complete {iterations} iterations, on average {duration/iterations} s/iter")
+        print(
+            f"Took {duration} seconds to complete {iterations} iterations, on average {duration/iterations} s/iter"
+        )
 
     print()
-    print(f"Average duration to complete {iterations} iterations:", sum(durations) / len(durations), "seconds")
+    print(
+        f"Average duration to complete {iterations} iterations:",
+        sum(durations) / len(durations),
+        "seconds",
+    )
     print(
         "Average duration per iteration ",
         ((sum(durations) / len(durations)) / iterations) * 1000,
