65d24320e7c3cbe34b41f171812e7c0cc9085b71
Andrew Jeffery <dev@jeffas.io>
2023-01-17 14:22:53 +0000

    Disable import error for pb2 files

--- benchmark/receipt_verify.py
+++ benchmark/receipt_verify.py
@@ -12,7 +12,9 @@ import json
 import timeit
 
 import ccf.receipt  # type: ignore
+# pylint: disable=import-error
 import etcd_pb2  # type: ignore
+# pylint: disable=import-error
 import lskvserver_pb2  # type: ignore
 from cryptography.x509 import load_pem_x509_certificate  # type: ignore
 from google.protobuf.json_format import ParseDict
